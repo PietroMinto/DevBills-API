e611648ae38e69bd39fda99ffad5b761b1895a8f
Pietro <pietrofulanminto@gmail.com>
2025-02-24 20:35:20 -0300

    arrumando erros

--- src/routes/transactions.route.ts
+++ src/routes/transactions.route.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 
 import { TransactionsController } from "../controllers/transactions.controller";
 import {
-	createTransactionSchema,
-	getDahBoardSchema,
-	getFinancialEvolutionSchema,
-	indexTransactionSchema,
+  createTransactionSchema,
+  getDahBoardSchema,
+  getFinancialEvolutionSchema,
+  indexTransactionSchema,
 } from "../dtos/transactions.dto";
 import { TransactionFactory } from "../factories/transactions.factory";
 import { ParamsType, validator } from "../middleware/validator.middlewares";
@@ -13,41 +13,41 @@ import { ParamsType, validator } from "../middleware/validator.middlewares";
 export const transactionsRoutes = Router();
 
 const controller = new TransactionsController(
-	TransactionFactory.getServiceInstance(),
+  TransactionFactory.getServiceInstance()
 );
 
 transactionsRoutes.post(
-	"/",
-	validator({
-		schema: createTransactionSchema,
-		type: ParamsType.BODY,
-	}),
-	controller.create,
+  "/",
+  validator({
+    schema: createTransactionSchema,
+    type: ParamsType.BODY,
+  }),
+  controller.create
 );
 
 transactionsRoutes.get(
-	"/",
-	validator({
-		schema: indexTransactionSchema,
-		type: ParamsType.QUERY,
-	}),
-	controller.index,
+  "/",
+  validator({
+    schema: indexTransactionSchema,
+    type: ParamsType.QUERY,
+  }),
+  controller.index
 );
 
 transactionsRoutes.get(
-	"/dashboard",
-	validator({
-		schema: getDahBoardSchema,
-		type: ParamsType.QUERY,
-	}),
-	controller.getDashboard,
+  "/dashboard",
+  validator({
+    schema: getDahBoardSchema,
+    type: ParamsType.QUERY,
+  }),
+  controller.getDashboard
 );
 
 transactionsRoutes.get(
-	"/financial-evolution",
-	validator({
-		schema: getFinancialEvolutionSchema,
-		type: ParamsType.QUERY,
-	}),
-	controller.getFinancialEvolution,
+  "/financial-evolution",
+  validator({
+    schema: getFinancialEvolutionSchema,
+    type: ParamsType.QUERY,
+  }),
+  controller.getFinancialEvolution
 );
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,40 @@
 import "dotenv/config";
-import express, { json } from "express";
+import express, {
+  type Express,
+  type Request,
+  type Response,
+  type NextFunction,
+} from "express";
+import cors from "cors";
 import { setupMongo } from "./database";
 import { errorHandler } from "./middleware/error-handler.middleware";
 import { routes } from "./routes";
-import cors from "cors";
 
-setupMongo().then(() => {
-  const app = express();
+const PORT = process.env.PORT || 3333;
+const FRONT_URL = process.env.FRONT_URL || "*";
 
-  app.use(
-    cors({
-      origin: process.env.FRONT_URL,
-    })
-  );
-  app.use(json());
-  app.use(routes);
-  app.use(errorHandler);
+setupMongo()
+  .then(() => {
+    const app: Express = express();
 
-  app.listen(3333, () => console.log("🚀 server running at port 3333!"));
-});
+    app.use(
+      cors({
+        origin: FRONT_URL,
+      })
+    );
+
+    app.use(express.json());
+
+    app.use(routes);
+
+    // Middleware de tratamento de erros
+    app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+    });
+
+    app.listen(PORT, () => console.log(`🚀 Server running at port ${PORT}!`));
+  })
+  .catch((error) => {
+    console.error("Error setting up the server:", error);
+  });
