83c8b5c0fcbbd12cf7e43686d65a2d0f8fd0f20e
Pietro <pietrofulanminto@gmail.com>
2025-02-24 19:29:20 -0300

    Melhorias para a Aplicação

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,19 @@ import express, { json } from "express";
 import { setupMongo } from "./database";
 import { errorHandler } from "./middleware/error-handler.middleware";
 import { routes } from "./routes";
-import cors from "cors"
+import cors from "cors";
 
 setupMongo().then(() => {
-	const app = express();
+  const app = express();
 
-	// app.use(cors({
-	// 	origin: process.env.FRONT_URL,
-	//  }),
-  //  );
-	app.use(json());
-	app.use(routes);
-	app.use(errorHandler);
+  app.use(
+    cors({
+      origin: process.env.FRONT_URL,
+    })
+  );
+  app.use(json());
+  app.use(routes);
+  app.use(errorHandler);
 
-	app.listen(3333, () => console.log("🚀 server running at port 3333!"));
-})
+  app.listen(3333, () => console.log("🚀 server running at port 3333!"));
+});
